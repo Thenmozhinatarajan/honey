9abe0443a213ca89f803bf801639632f8db60a64
Thenmozhinatarajan <46585082+Thenmozhinatarajan@users.noreply.github.com>
2019-01-11 09:46:09 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class sum
+{
+	public static void main(String args[])
+	{
+		Scanner in=new Scanner(System.in);
+		int N=in.nextInt();
+		
+		{	int Sum;
+			Sum=N*(N+1)/2;
+			System.out.println(Sum);
+		}
+	}
+}
