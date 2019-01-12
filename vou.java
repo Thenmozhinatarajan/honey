6a5455a823faac6b258bf01aa6b127ee4603de22
Thenmozhinatarajan <46585082+Thenmozhinatarajan@users.noreply.github.com>
2019-01-12 18:45:06 +0530

    Update vou.java

--- vou.java
+++ vou.java
@@ -9,6 +9,10 @@ class vou
 		{
 			System.out.println("Vowel");
 		}
+		else if(c=='b'||c=='c'||c=='d'||c=='f'||c=='g'||c=='h'||c=='j'||c=='k'||c=='l'||c=='m'||c=='n'||c=='p'||c=='q'||c=='r'||c=='s'||c=='t'||c=='v'||c=='w'||c=='x'||c=='y'||c=='z')
+		{
+			System.out.println("Consonant");
+		}
 		else
 		{
 			System.out.println("invalid");
