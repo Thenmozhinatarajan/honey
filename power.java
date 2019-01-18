45a86dec272ab288a3b1e12ee4f9af950f971bfb
Thenmozhinatarajan <46585082+Thenmozhinatarajan@users.noreply.github.com>
2019-01-18 09:35:14 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+class power
+{
+	public static void main(String args[])
+	{
+	Scanner in=new Scanner(System.in);
+	int base=in.nextInt();
+	int exponent=in.nextInt();
+	int result=1;
+	while (exponent != 0)
+    {
+	    result=result*base;
+	    --exponent;
+	}
+	System.out.println(result);
+	}
+}
