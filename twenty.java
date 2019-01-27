369da92f24afc120de0ca48cc26abc674cb68090
Thenmozhinatarajan <46585082+Thenmozhinatarajan@users.noreply.github.com>
2019-01-27 15:41:36 +0530

    Create twenty.java

---
+++ twenty.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class twenty
+{
+	public static void main(String args[])
+	{
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int ans=0;
+		for(int i=1;i<=5;i++)
+		{
+			System.out.println(n*i);
+		}
+		
+	}
+}
+	
