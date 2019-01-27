f6497e6f583d54744d61a5c074c2c9d54836a63c
Thenmozhinatarajan <46585082+Thenmozhinatarajan@users.noreply.github.com>
2019-01-27 15:22:44 +0530

    Create eighteen.java

---
+++ eighteen.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+class eighteen
+{
+	public static void main(String args[])
+	{
+		Scanner in=new Scanner(System.in);
+		int low=in.nextInt();
+		int high=in.nextInt();
+
+        for(int number = low + 1; number < high; ++number)
+        {
+
+            if (checkArmstrong(number))
+                System.out.print(number + " ");
+        }
+    }
+
+    public static boolean checkArmstrong(int num) 
+    {
+        int digits = 0;
+        int result = 0;
+      
+        int originalNumber = num;
+
+        // number of digits calculation
+        while (originalNumber != 0) {
+            originalNumber /= 10;
+            ++digits;
+        }
+
+        originalNumber = num;
+
+        
+        while (originalNumber != 0)
+        {
+            int remainder = originalNumber % 10;
+            result += Math.pow(remainder, digits);
+            originalNumber /= 10;
+        }
+
+        if (result == num)
+            return true;
+
+        return false;
+    }
+}
